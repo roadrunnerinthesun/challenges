c60e984fa381ecfaef23c4c34fd89ec86caec58c
Sahara <saharabegom@gmail.com>
2022-06-07 16:14:43 +0000

    In Progress

--- merge_unsorted.py
+++ merge_unsorted.py
@@ -3,3 +3,24 @@ def sort_ab(a, b):
 
     You may not use sorted() or .sort().
     """
+a = [1, 3, 5, 7]
+b = [2, 6, 8, 10]
+c = []
+
+c = a+b
+print ("unsorted:", c)
+c = sorted(c)
+print("sorted:", c)
+
+i, j = 0,0,
+c = []
+
+while (i>j):
+    c = a[i]
+    i = += 1
+    j = += 1
+    while (j<i):
+        c = b[i]
+        i = += 1
+        j += 1
+    
